3e77b276d1a00b341d805f4db2873ad1fd26aba7
sunjiangang666 <33009922+sunjiangang666@users.noreply.github.com>
2018-02-13 15:01:17 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -19,7 +19,7 @@ function serch_out(){
 	but.style.backgroundColor="#996699";	
 	
 }
-var placec;
+//获取当前时间函数
 function rilitime(){
 	var date=new Date();
 	var h=date.getHours();
@@ -36,19 +36,6 @@ function rilitime(){
 	var timemonth=document.getElementById("time_month").innerHTML=yue+1+"月"+ri+"日"+"&nbsp;&nbsp;&nbsp;&nbsp;"+"星期"+day;	
 	 t=setTimeout("rilitime()",1000);
 }
-function placel(){
-	if(placec==null){
-		return;
-	}
-	document.getElementById("place_c").innerHTML=--placec;	
-}
-function placer(){
-	if(placec==null){
-		return;
-	}
-	document.getElementById("place_c").innerHTML=++placec;
-}
-
 
 
 function riqi(i){
@@ -91,6 +78,7 @@ function but_right_out(){
 	var right=document.getElementById("but_right");
 	right.style.opacity="0.3";	
 }
+
 var lunbok; // 图片 div
 var butleft;
 var butright;
@@ -101,8 +89,8 @@ var li;
 var curPositon = 0; // 当前的图片位置
 var dotArray; // 点 li 数组
 window.onload=function(){
+	
 	dotArray = document.getElementById("dot_ul").getElementsByTagName("li");
-	 placec=document.getElementById("place_c").innerHTML=new Date().getFullYear();
 	rilitime();
 	lunbok=document.getElementById('lunbo_k');
 	butleft=document.getElementById('but_left');
@@ -119,7 +107,6 @@ window.onload=function(){
 	lunbo.onmouseout=play;
 	lunbo.onmouseover=stop;	
 	var dot0 = document.getElementById("dot0");
-	
 		dot0.onclick = function(){
 			curPositon = 0;
 			onFocusChanged();
@@ -152,10 +139,8 @@ window.onload=function(){
             限制超过的功能就失效了。用这个方法，即可解决这个问题。注：这两个方法用于onmousedown和onmouseup中*/  
             if(typeof spin.setCapture!='undefined'){
             	spin.setCapture();
-            }
-            
+            }           
             document.onmousemove= function(e){
-            	
             	var e = e || window.event;//兼容ie浏览器
             	var left = e.clientX-diffX;
             	var top = e.clientY-diffY;
@@ -185,8 +170,205 @@ window.onload=function(){
             		}
             	}
             }
-		}
-}		
+		}	
+			var d1 = new Date();  // 获取当前系统时间 我现在的时间是 2016年4月25日 星期一
+d1.getFullYear();    // 获取年信息， 2016
+d1.getMonth();      // 获取月信息（从0开始 范围：0-11） 3
+d1.getDate();      // 获取天信息 此处结果：25
+d1.getDay();      // 获取星期信息 （0-6） 此处结果： 1 
+//# 设置 2016年3月15日
+//var d2 = new Date(2016, 2, 15);    // 月是从0开始计数， 需要减一
+//d2.getFullYear();          // 2016
+//d2.getMonth();            // 2
+//d2.getDate();            // 15
+//d2.toLocaleDateString();      // "2016/3/15" 证明设置正确 
+var d3 = new Date(2016, 3, 30);
+d3.toLocaleDateString();      // "2016/4/30"
+var d4 = new Date(2016, 3, 31);
+d4.toLocaleDateString();      // "2016/5/1"
+var d5 = new Date(2016, 3, 33);
+d5.toLocaleDateString();      // "2016/5/3"
+var d6 = new Date(2016, 4, 1);
+d6.toLocaleDateString();      // "2016/5/1"
+var d7 = new Date(2016, 4, 0);
+d7.toLocaleDateString();      // "2016/4/30"
+var d8 = new Date(2016, 4, -3);
+d8.toLocaleDateString();      // "2016/4/27"
+
+ (function(){
+  /*
+   * 用于记录日期，显示的时候，根据dateObj中的日期的年月显示
+   */
+  var dateObj = (function(){
+    var _date = new Date();    // 默认为当前系统时间
+    return {
+      getDate : function(){
+        return _date;
+      },
+      setDate : function(date) {
+        _date = date;
+      }
+    };
+  })();
+ 
+  // 设置calendar div中的html部分
+  renderHtml();
+  // 表格中显示日期
+  showCalendarData();
+  // 绑定事件
+  bindEvent();
+ 
+  /**
+   * 渲染html结构
+   */
+  function renderHtml() {
+    var calendar = document.getElementById("calendar");
+    var titleBox = document.createElement("div");  // 标题盒子 设置上一月 下一月 标题
+    var bodyBox = document.createElement("div");  // 表格区 显示数据
+ 
+    // 设置标题盒子中的html
+    titleBox.className = 'calendar-title-box';
+    titleBox.innerHTML = "<span class='prev-month' id='prevMonth'></span>" +
+      "<span class='calendar-title' id='calendarTitle'></span>" +
+      "<span id='nextMonth' class='next-month'></span>";
+    calendar.appendChild(titleBox);    // 添加到calendar div中
+ 
+    // 设置表格区的html结构
+    bodyBox.className = 'calendar-body-box';
+    var _headHtml = "<tr>" + 
+              "<th>日</th>" +
+              "<th>一</th>" +
+              "<th>二</th>" +
+              "<th>三</th>" +
+              "<th>四</th>" +
+              "<th>五</th>" +
+              "<th>六</th>" +
+            "</tr>";
+    var _bodyHtml = "";
+ 
+    // 一个月最多31天，所以一个月最多占6行表格
+    for(var i = 0; i < 6; i++) {  
+      _bodyHtml += "<tr>" +
+              "<td></td>" +
+              "<td></td>" +
+              "<td></td>" +
+              "<td></td>" +
+              "<td></td>" +
+              "<td></td>" +
+              "<td></td>" +
+            "</tr>";
+    }
+    bodyBox.innerHTML = "<table id='calendarTable' class='calendar-table'>" +
+              _headHtml + _bodyHtml +
+              "</table>";
+    // 添加到calendar div中
+    calendar.appendChild(bodyBox);
+  }
+ 
+  /**
+   * 表格中显示数据，并设置类名
+   */
+  function showCalendarData() {
+    var _year = dateObj.getDate().getFullYear();
+    var _month = dateObj.getDate().getMonth() + 1;
+    var _dateStr = getDateStr(dateObj.getDate());
+ 
+     //  设置顶部标题栏中的 年、月信息
+    var calendarTitle = document.getElementById("calendarTitle");
+    var titleStr = _dateStr.substr(0, 4) + "年" + _dateStr.substr(4,2) + "月";
+    calendarTitle.innerText = titleStr;
+ 
+    // 设置表格中的日期数据
+    var _table = document.getElementById("calendarTable");
+    var _tds = _table.getElementsByTagName("td");
+    var _firstDay = new Date(_year, _month - 1, 1);  // 当前月第一天
+    for(var i = 0; i < _tds.length; i++) {
+      var _thisDay = new Date(_year, _month - 1, i + 1 - _firstDay.getDay());
+      var _thisDayStr = getDateStr(_thisDay);
+      _tds[i].innerText = _thisDay.getDate();
+      //_tds[i].data = _thisDayStr;
+      _tds[i].setAttribute('data', _thisDayStr);
+      if(_thisDayStr == getDateStr(new Date())) {    // 当前天
+        _tds[i].className = 'currentDay';
+      }else if(_thisDayStr.substr(0, 6) == getDateStr(_firstDay).substr(0, 6)) {
+        _tds[i].className = 'currentMonth';  // 当前月
+      }else {    // 其他月
+        _tds[i].className = 'otherMonth';
+      }
+    }
+  }
+ 
+  /**
+   * 绑定上个月下个月事件
+   */
+  function bindEvent() {
+    var prevMonth = document.getElementById("prevMonth");
+    var nextMonth = document.getElementById("nextMonth");
+    addEvent(prevMonth, 'click', toPrevMonth);
+    addEvent(nextMonth, 'click', toNextMonth);
+    var table = document.getElementById("calendarTable");
+var tds = table.getElementsByTagName('td');
+for(var i = 0; i < tds.length; i++) {
+  addEvent(tds[i], 'click', function(e){
+    console.log(e.target.getAttribute('data'));
+  });
+}
+  }
+ 
+  /**
+   * 绑定事件
+   */
+  function addEvent(dom, eType, func) {
+    if(dom.addEventListener) {  // DOM 2.0
+      dom.addEventListener(eType, function(e){
+        func(e);
+      });
+    } else if(dom.attachEvent){  // IE5+
+      dom.attachEvent('on' + eType, function(e){
+        func(e);
+      });
+    } else {  // DOM 0
+      dom['on' + eType] = function(e) {
+        func(e);
+      }
+    }
+    
+  }
+ 
+  /**
+   * 点击上个月图标触发
+   */
+  function toPrevMonth() {
+    var date = dateObj.getDate();
+    dateObj.setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
+    showCalendarData();
+  }
+ 
+  /**
+   * 点击下个月图标触发
+   */
+  function toNextMonth() {
+    var date = dateObj.getDate();
+    dateObj.setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
+    showCalendarData();
+  }
+ 
+  /**
+   * 日期转化为字符串， 4位年+2位月+2位日
+   */
+  function getDateStr(date) {
+    var _year = date.getFullYear();
+    var _month = date.getMonth() + 1;    // 月从0开始计数
+    var _d = date.getDate();
+     
+    _month = (_month > 9) ? ("" + _month) : ("0" + _month);
+    _d = (_d > 9) ? ("" + _d) : ("0" + _d);
+    return _year + _month + _d;
+  }
+})();
+		
+}	
+
 	function play(){//定时器
 		timer=setInterval(function(){
 			next()
@@ -196,21 +378,6 @@ window.onload=function(){
 	function stop(){		    
 		clearInterval(timer); //window.clearInterval解除定时器
 	}	
-//	function animate(offset){
-//		if (lunbok.style.left=="") {
-//		var newleft=0+offset;
-//		lunbok.style.left=newleft+'px';
-//		}
-//		var newleft=parseInt(lunbok.style.left)+offset;
-//		lunbok.style.left=newleft+'px';
-//		
-//		if(newleft<-1200){
-//			lunbok.style.left=0+ "px";
-//		}
-//		if(newleft>-400){
-//			lunbok.style.left=-1200+"px";
-//		}
-//	}	
 	// 焦点位置发生改变
 	function onFocusChanged(){
 		for (var i=0;i<dotArray.length;i++) {
@@ -223,19 +390,6 @@ window.onload=function(){
 //	var offset=0; // 图片移动左间距
 	// 图片位置发生改变
 	function onImageChanged(){
-//		if(curPositon==0){
-//			offset=0;
-//			lunbok.style.left = offset+"px";
-//			clearTimer();
-//			return;
-//		}
-//		if(imgChangedTimer != null){
-//			clearTimer();
-//			offset=(-(curPositon%4))*400;
-//			lunbok.style.left = offset+"px";
-//			return;
-//		}
-
 		var offset = -curPositon*400;
 		lunbok.style.left = offset+"px";
 	}
@@ -254,41 +408,7 @@ window.onload=function(){
 		}
 		curPositon = curPositon%4;
 		onFocusChanged();
-//		preAnime();
 	}
-	// 下一个动画
-//	function nextAnime(){
-//		imgChangedTimer = setInterval(function(){
-//		var maxOffset = -curPositon*400;
-//		offset-=4;
-//		lunbok.style.left = offset+"px";
-//		if(offset<=maxOffset){
-//				// 清除定时器
-//				clearTimer();
-//				offset = maxOffset;
-//				return;
-//			}
-//		},5);
-//	}
-	// 上一个动画
-//	function preAnime(){
-//		imgChangedTimer = setInterval(function(){
-//		var maxOffset = -curPositon*400;
-//		offset+=4;
-//		lunbok.style.left = offset+"px";
-//		if(offset>=maxOffset){
-//				// 清除定时器
-//				clearTimer();
-//				offset = maxOffset;
-//				return;
-//			}
-//		},5);
-//	}
-	// 清除动画定时器
-//	function clearTimer(){
-//				clearInterval(imgChangedTimer);
-//				imgChangedTimer =null;
-//	}
 
 	
 function dropdown(li){
@@ -306,11 +426,5 @@ function dropdown(li){
 //元素.setAttribute() //增加,更改属性,有两个参数,第一个参数是属性名,第二个是值.
 
 //元素.getAttribute() //传入属性名,获得该属性对应的值
-
-
-//function dropdown(li){
-//	var updown=li.getElementsByTagName('ul')[0];
-//	updown.style.display="none";
-//}
 
 
